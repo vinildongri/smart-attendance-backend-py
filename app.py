e08cc3c51496ea3ee90df70c48179e8645296c13
Vinil Dongri <dongrivinil@gmail.com>
2026-04-19 00:17:57 +0530

    change port

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def recognize():
 
 # Render needs to be able to bind to the dynamic PORT environment variable
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
